15f0cdeca10169b81875383829d41807715a835c
VictoriaZaias <victoriazaias@outlook.com>
2023-11-09 14:58:51 -0300

    Documentação
    - Documento README começado;
    - Documentação do código existente.

--- trabalho1.pl
+++ trabalho1.pl
@@ -1,4 +1,5 @@
-% Predicado para ler um arquivo, passando o nome dele.
+% ler_arquivo(NomeArquivo): Tenta ler arquivo no caminho passado em NomeArquivo
+% ler_arquivo(+NomeArquivo)
 ler_arquivo(NomeArquivo):-
     % Abre o arquivo e armazena o fluxo (stream) do arquivo na variável Stream.
     open(NomeArquivo, read, Stream),
@@ -12,11 +13,13 @@ ler_arquivo(NomeArquivo):-
     % Processa a lógica do trabalho pedido, fornecendo as linhas lidas do arquivo.
     processa_linhas(Linhas).
 
-% Se atingiu o final do arquivo, retorna uma lista vazia.
+% ler_linhas(Stream, Lista): Se atingiu o final do arquivo, retorna uma lista vazia
+% ler_linhas(+Stream, ?Lista)
 ler_linhas(Stream, []):-
     at_end_of_stream(Stream).
 
-% Se não atingiu fim do arquivo, lê as linhas do arquivo e constrói uma lista de linhas.
+% ler_linhas(Stream, Lista): Se não atingiu fim do arquivo, lê as linhas do arquivo e constrói uma lista de linhas
+% ler_linhas(+Stream, ?Lista)
 ler_linhas(Stream, [Linha | Resto]):-
     % Se não chegou no fim do arquivo, continua.
     \+ at_end_of_stream(Stream),
@@ -27,9 +30,9 @@ ler_linhas(Stream, [Linha | Resto]):-
     % Pede para continuar lendo o arquivo e ir armazenando no resto da lista, pois a cabeça já tá ocupada (Linha).
     ler_linhas(Stream, Resto).
 
-% Caso base da recursividade.
+% processa_linhas(Lista): Printa cada elemento da lista
+% processa_linhas(+Lista)
 processa_linhas([]).
-% Caso recursivo, para printar cada elemento do lista Linha.
 processa_linhas([Linha | Resto]):-
     % Printa a cabeça da lista.
     writeln(Linha),
